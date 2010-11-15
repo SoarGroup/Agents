54da9540b112e8dd9ba30fd8281a9156053ee6e2
Nate Derbinsky <nate.derbinsky@gmail.com>
2010-11-15 16:15:16 +0000

    improved default index page

--- speedy/reporting/web/index.php
+++ speedy/reporting/web/index.php
@@ -4,41 +4,83 @@
 		require 'common/private/start.inc.php';
 		
 		$page_info['title'] = 'Reports';
+		$page_info['head'] = jquery_tabs( 'tabs' );
 	}
 	
 ?>
 
 	<?php
 		$exps = exp_list();
-		$reports = array(
-			
-			5 => array(
-				'test_report1.php' => 'Sample 1',
-				'test_report2.php' => 'Sample 2',
-				'test_report3.php' => 'Sample 3',
-				'test_report4.php' => 'Sample 4',
-			)
-			
-		);
 		
-		foreach ( $exps as $exp_id => $exp_name )
+		// tabs -> experiment -> (file->title)
+		$reports = array();
+		@include( 'index.inc.php' );
+		
+		if ( !empty( $reports ) )
 		{
-			if ( isset( $reports[ $exp_id ] ) )
-			{
-				echo '<div class="section">';
-					echo '<div class="title">';
-						echo htmlentities( $exp_name );
-					echo '</div>';
-					echo '<div class="body">';
-						echo '<ul>';
-							foreach ( $reports[ $exp_id ] as $file_name => $report_name )
+			echo '<div id="tabs">';
+			
+				echo '<ul>';
+				
+					$tab_counter = 0;
+					foreach ( $reports as $tab_id => $reps )
+					{
+						echo '<li><a href="#tabs-' . htmlentities( $tab_counter++ ) . '">' . htmlentities( $tab_id ) . '</a></li>';
+					}
+				
+				echo '</ul>';
+				
+				$tab_counter = 0;
+				foreach ( $reports as $tab_id => $reps )
+				{
+					echo '<div id="tabs-' . htmlentities( $tab_counter++ ) . '">';
+					
+						if ( !empty( $reps ) )
+						{
+							foreach ( $exps as $exp_id => $exp_name )
 							{
-								echo '<li><a href="' . htmlentities( $file_name ) . '">' . htmlentities( $report_name ) . '</a></li>';
+								if ( isset( $reps[ $exp_id ] ) )
+								{
+									echo '<div class="section">';
+									
+									echo '<div class="title">';
+										echo htmlentities( $exp_name );
+									echo '</div>';
+									
+									echo '<div class="body">';
+										echo '<ul>';
+											foreach ( $reps[ $exp_id ] as $file_name => $report_name )
+											{
+												echo '<li><a href="' . htmlentities( $file_name ) . '">' . htmlentities( $report_name ) . '</a></li>';
+											}
+										echo '</ul>';
+									echo '</div>';
+									
+									echo '</div>';
+								}
 							}
-						echo '</ul>';
+						}
+					else
+					{
+						echo '<div class="section">';
+							echo '<div class="body">';
+								echo '<p>No reports found.</p>';
+							echo '</div>';
+						echo '</div>';
+					}
+					
 					echo '</div>';
+				}
+			
+			echo '</div>';
+		}
+		else
+		{
+			echo '<div class="section">';
+				echo '<div class="body">';
+					echo '<p>No reports found.</p>';
 				echo '</div>';
-			}
+			echo '</div>';
 		}
 	?>
 
